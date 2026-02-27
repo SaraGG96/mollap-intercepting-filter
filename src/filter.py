1ba98f385f192c1732cd737bf1a22971d722c40d
Sara <saragarciagarrido96@gmail.com>
2026-02-27 13:11:12 +0100

    feature: Añadir la clase Autentificación (Autenticacio)

--- src/filter.py
+++ src/filter.py
@@ -1,3 +1,8 @@
 class Filter:
     def execucio(self, username):
         pass
+
+
+class Autenticacio(Filter):
+    def execucio(self, username):
+        print(f"Autenticación OK para {username}")
