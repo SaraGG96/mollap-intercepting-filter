5d0196cd6866d081d7b5001422f95c52dfb2cd4c
Sara <saragarciagarrido96@gmail.com>
2026-02-27 13:11:33 +0100

    feature: Añadir clase Autoritzacio (Autorización)

--- src/filter.py
+++ src/filter.py
@@ -6,3 +6,8 @@ class Filter:
 class Autenticacio(Filter):
     def execucio(self, username):
         print(f"Autenticación OK para {username}")
+
+
+class Autoritzacio(Filter):
+    def execucio(self, username):
+        print(f"Autorización OK para {username}")
