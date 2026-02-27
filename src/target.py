50ce5340985196384409c010bd21e25eaa92b0b0
Sara <saragarciagarrido96@gmail.com>
2026-02-27 13:09:26 +0100

    feature: Añadir clase Vehicle

--- src/target.py
+++ src/target.py
@@ -2,3 +2,9 @@ class Target:
     
     def execucio(self, username):
         pass
+
+
+class Vehicle(Target):
+    
+    def execucio(self, username):
+        print(f"Puerta abierta {username}!")
