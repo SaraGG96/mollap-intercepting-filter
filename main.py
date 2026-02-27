ca40a4edc821218d0766ecd500fdf01743972650
Sara <saragarciagarrido96@gmail.com>
2026-02-27 13:26:17 +0100

    feat(main.py): :zap: Inicializar main, añadir importaciones

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 
+from src.target import Vehicle
+from src.filter import Autenticacio, Autoritzacio
+from src.programador_tasques import ProgramadorTasques
+from src.mollapp import Mollapp
+
+
+def main():
+    
+
+
+
+if __name__ == "__main__":
+    main()
